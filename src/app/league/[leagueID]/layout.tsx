3fa6e6885d0c63f49bb355b4156883eb459e8b59
Sam Caldwell <samcaldwell19@gmail.com>
2024-08-07 12:10:12 -0400

    only show completed drafts in sidebar

--- src/app/league/[leagueID]/layout.tsx
+++ src/app/league/[leagueID]/layout.tsx
@@ -1,19 +1,13 @@
 'use client';
 import Sidebar from '../Sidebar';
-import { fetchLeagueHistory } from '@/espn/league';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { makeApiRequest } from '@/app/api/utils';
-import { FetchLeagueHistoryRequest, FetchLeagueHistoryResponse } from '@/app/api/fetch-league-history/interface';
-import { FetchDraftResponse } from '@/app/api/fetch-draft/interface';
-import { FETCH_LEAGUE_HISTORY_ENDPOINT } from '@/app/api/interface';
 import ApiClient from '@/app/api/ApiClient';
 
 const DEFAULT_YEAR = 2024;
 
 const LeagueLayout = ({ children, params } : { children: React.ReactNode, params: {leagueID: string, draftYear?: string } }) => {
     const leagueID = parseInt(params.leagueID);
-    // const leagueHistory = await fetchLeagueHistory(leagueID, DEFAULT_YEAR, auth);
 
     const [prevAuctions, setPrevAuctions] = useState<number[]>([]);
     const [leagueName, setLeagueName] = useState<string>('');
@@ -28,22 +22,14 @@ const LeagueLayout = ({ children, params } : { children: React.ReactNode, params
                     useRouter().push('/');
                     return;
                 }
-                // const leagueHistory = await fetchLeagueHistory(leagueID, DEFAULT_YEAR, auth);
-                // const router = useRouter();
-                // if (leagueHistory.size === 0) {
-                //     console.log('League Layout: failed to load league history');
-                //     console.log(auth);
-                //     router.push('/');
-                //     return;
-                // }
-
+                
                 const leagueHistory = resp.data!;
                 const auctions: number[] = [];
                 for (const [year, info] of Object.entries(leagueHistory)) {
                     if (typeof info === 'number') {
                         console.error(`Failed to fetch league info for ${year}: ${info}`);
                     } else {
-                        if (info.settings.draftSettings.type === 'AUCTION') {
+                        if (info.draftDetail.drafted && info.settings.draftSettings.type === 'AUCTION') {
                             auctions.push(parseInt(year));
                         }
                     }
@@ -52,9 +38,8 @@ const LeagueLayout = ({ children, params } : { children: React.ReactNode, params
                 setPrevAuctions(auctions);
                 setLeagueName(leagueHistory[DEFAULT_YEAR]!.settings.name);
             } catch (error: any) {
-                // setError(error.message);
+                alert(`Error while loading league history: ${error.message}`);
             } finally {
-                // setLoading(false);
             }
         };
 
