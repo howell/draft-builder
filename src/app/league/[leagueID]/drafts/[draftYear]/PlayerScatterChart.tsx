ab3e57e632dfa8c515da5f89a029d5426e455479
Sam Caldwell <samcaldwell19@gmail.com>
2024-08-13 19:37:19 -0400

    don't repeat a bunch of computation for every render

--- src/app/league/[leagueID]/drafts/[draftYear]/PlayerScatterChart.tsx
+++ src/app/league/[leagueID]/drafts/[draftYear]/PlayerScatterChart.tsx
@@ -6,34 +6,16 @@ import { findBestRegression, predictPrice } from '@/app/league/analytics';
 import { Dot } from 'recharts';
 import { ReactElement, useState } from 'react';
 
+export type ChartData = {
+    data: TableData[];
+    mse: number;
+    topMse: number;
+}
+
 const PlayerScatterChart : React.FC<{data: TableData[]}> = ({ data }) => {
     const [dataStart, setDataStart] = useState(0);
     const [dataEnd, setDataEnd] = useState(data.length);
-    const chartData = data.sort((a, b) => b.auctionPrice - a.auctionPrice)
-        .map((v, i) => ({
-            ...v,
-            index: i,
-            tooltip: `${i}: ${v.name} (${v.position}), \$${v.auctionPrice}`
-        }));
-    const regressionData = chartData.map(v => [v.index, v.auctionPrice] as [number, number]);
-    const result = findBestRegression(regressionData);
-    const withPredictions = chartData.map((v, i) => {
-        const prediction = predictPrice(result, i);
-        const delta = prediction - v.auctionPrice;
-        return {
-             ...v,
-            prediction,
-            tooltip: `${i}: ${v.name} (${v.position}), \$${v.auctionPrice}`,
-            desc: `predicted: ${prediction} (err=${delta})`
-        }
-    });
-    const referencePoints = result.points.map(p => ({ x: p[0], y: p[1] }));
-    const mse = meanSquaredError(withPredictions, d => d.auctionPrice, d => d.prediction)
-    const topMse = meanSquaredError(withPredictions.slice(0, 50), d => d.auctionPrice, d => d.prediction);
-    console.log(referencePoints)
-    console.log("Predictions: ", withPredictions[0])
-    console.log("MSE: ", mse)
-    console.log("Top 50 MSE: ", topMse);
+    const [chartData, _setChartData] = useState(() => initializeData(data));
 
     return (
         <div className='flex flex-col w-full m-auto'>
@@ -44,11 +26,11 @@ const PlayerScatterChart : React.FC<{data: TableData[]}> = ({ data }) => {
                 </div>
                 <div className='flex flex-col w-full m-auto items-center justify-center'>
                     <h2 className='text-xl'>Actual and Predicted Player Prices</h2>
-                    <h2 className='text-lg'>Mean Squared Error = {mse} (Top 50 MSE: {topMse})</h2>
+                    <h2 className='text-lg'>Mean Squared Error = {chartData.mse} (Top 50 MSE: {chartData.topMse})</h2>
                 </div>
             </div>
             <ResponsiveContainer width="90%" height={600}>
-                <LineChart data={withPredictions}>
+                <LineChart data={chartData.data}>
                     <CartesianGrid />
                     <XAxis dataKey="index" type="number" allowDataOverflow domain={[dataStart, dataEnd]} />
                     <YAxis />
@@ -63,6 +45,35 @@ const PlayerScatterChart : React.FC<{data: TableData[]}> = ({ data }) => {
 }
 
 export default PlayerScatterChart;
+
+function initializeData(data: TableData[]): ChartData {
+    const chartData = data.sort((a, b) => b.auctionPrice - a.auctionPrice)
+        .map((v, i) => ({
+            ...v,
+            index: i,
+            tooltip: `${i}: ${v.name} (${v.position}), \$${v.auctionPrice}`
+        }));
+    const regressionData = chartData.map(v => [v.index, v.auctionPrice] as [number, number]);
+    const result = findBestRegression(regressionData);
+    const withPredictions = chartData.map((v, i) => {
+        const prediction = predictPrice(result, i);
+        const delta = prediction - v.auctionPrice;
+        return {
+            ...v,
+            prediction,
+            tooltip: `${i}: ${v.name} (${v.position}), \$${v.auctionPrice}`,
+            desc: `predicted: ${prediction} (err=${delta})`
+        }
+    });
+    const referencePoints = result.points.map(p => ({ x: p[0], y: p[1] }));
+    const mse = meanSquaredError(withPredictions, d => d.auctionPrice, d => d.prediction)
+    const topMse = meanSquaredError(withPredictions.slice(0, 50), d => d.auctionPrice, d => d.prediction);
+    return {
+        data: withPredictions,
+        mse,
+        topMse
+    };
+}
 
 const CustomTooltip = ({ active, payload }: any) => {
     if (active && payload && payload.length) {
