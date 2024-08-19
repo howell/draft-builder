6fa1f5849c51f6ef100cb7d1fb8b7d4874c9d542
Sam Caldwell <samcaldwell19@gmail.com>
2024-08-19 10:24:20 -0400

    more tooltip tests

--- src/ui/Tooltip.test.tsx
+++ src/ui/Tooltip.test.tsx
@@ -28,7 +28,7 @@ describe('Tooltip', () => {
         expect(queryByText('Tooltip Text')).toBeNull();
     });
 
-    it('should show the tooltip on mouse enter', () => {
+    it('should show the tooltip on mouse enter', async () => {
         const { getByText, getByTestId, getByRole } = render(
             <Tooltip text="Tooltip Text">
                 <p data-testid="in-tooltip">stuff</p>
@@ -37,13 +37,13 @@ describe('Tooltip', () => {
         );
 
         fireEvent.mouseEnter(getByRole('tooltip-trigger'));
-        tooltipWait();
+        await tooltipWait();
 
         expect(getByText('Tooltip Text')).toBeInTheDocument();
     });
 
     it('should hide the tooltip on mouse leave', async () => {
-        const { getByTestId, queryByText } = render(
+        const { getByRole, queryByText } = render(
             <div>
                 <p data-testid="outside-tooltip">stuff</p>
                 <Tooltip text="Tooltip Text">
@@ -52,15 +52,16 @@ describe('Tooltip', () => {
             </div>
         );
 
-        fireEvent.mouseEnter(getByTestId('in-tooltip'));
-        tooltipWait();
-        fireEvent.mouseLeave(getByTestId('in-tooltip'));
-        tooltipWait();
+        fireEvent.mouseEnter(getByRole('tooltip-trigger'));
+        await tooltipWait();
+        expect(queryByText('Tooltip Text')).toBeInTheDocument();
+        fireEvent.mouseLeave(getByRole('tooltip-trigger'));
+        await tooltipWait();
 
         expect(queryByText('Tooltip Text')).toBeNull();
     });
 
-    it('should show the tooltip on touch start', () => {
+    it('should show the tooltip on touch start', async () => {
         const { getByText, getByRole } = render(
             <Tooltip text="Tooltip Text">
                 <button data-testid="in-tooltip">Button</button>
@@ -68,23 +69,88 @@ describe('Tooltip', () => {
         );
 
         fireEvent.mouseEnter(getByRole('tooltip-trigger'));
-        tooltipWait();
+        await tooltipWait();
 
         expect(getByText('Tooltip Text')).toBeInTheDocument();
     });
 
     it('should hide the tooltip on touch end', async () => {
-        const { getByText, getByTestId, queryByText } = render(
+        const { getByRole, queryByText } = render(
             <Tooltip text="Tooltip Text">
                 <button data-testid="in-tooltip">Button</button>
             </Tooltip>
         );
-
-        fireEvent.touchStart(getByTestId('in-tooltip'));
-        tooltipWait();
-        fireEvent.touchEnd(getByTestId('in-tooltip'));
-        tooltipWait();
-
-        expect(queryByText('Tooltip Text')).toBeNull();
+    
+        fireEvent.touchStart(getByRole('tooltip-trigger'));
+        await tooltipWait();
+        fireEvent.touchEnd(getByRole('tooltip-trigger'));
+        await tooltipWait();
+    
+        expect(queryByText('Tooltip Text')).not.toBeInTheDocument();
     });
+});
+it('should position the tooltip to the top right if there is enough room', async () => {
+    const { getByRole, queryByText } = render(
+        <div style={{ position: 'relative', width: '200px', height: '200px' }}>
+            <Tooltip text="Tooltip Text">
+                <button data-testid="tooltip-trigger">Button</button>
+            </Tooltip>
+        </div>
+    );
+
+    const tooltipTrigger = getByRole('tooltip-trigger');
+    fireEvent.mouseEnter(tooltipTrigger);
+    await tooltipWait();
+
+    const tooltip = queryByText('Tooltip Text');
+    const tooltipRect = tooltip?.getBoundingClientRect();
+    const triggerRect = tooltipTrigger.getBoundingClientRect();
+
+    expect(tooltip).toBeInTheDocument();
+    expect(tooltipRect?.top).toBeLessThanOrEqual(triggerRect.top);
+    expect(tooltipRect?.right).toBeGreaterThanOrEqual(triggerRect.right);
+});
+
+it('should adjust the tooltip position down if there is not enough room on the top', async () => {
+    const { getByRole, queryByText } = render(
+        <div style={{ position: 'relative', width: '200px', height: '50px' }}>
+            <Tooltip text="Tooltip Text">
+                <button data-testid="tooltip-trigger">Button</button>
+            </Tooltip>
+        </div>
+    );
+
+    const tooltipTrigger = getByRole('tooltip-trigger');
+    fireEvent.mouseEnter(tooltipTrigger);
+    await tooltipWait();
+
+    const tooltip = queryByText('Tooltip Text');
+    const tooltipRect = tooltip?.getBoundingClientRect();
+    const triggerRect = tooltipTrigger.getBoundingClientRect();
+
+    expect(tooltip).toBeInTheDocument();
+    expect(tooltipRect?.top).toBeGreaterThanOrEqual(triggerRect.bottom);
+    expect(tooltipRect?.right).toBeGreaterThanOrEqual(triggerRect.right);
+});
+
+it('should adjust the tooltip position to the left if there is not enough room on the right', async () => {
+    const { getByRole, queryByText } = render(
+        <div style={{ position: 'relative', width: '50px', height: '200px' }}>
+            <Tooltip text="Tooltip Text">
+                <button data-testid="tooltip-trigger">Button</button>
+            </Tooltip>
+        </div>
+    );
+
+    const tooltipTrigger = getByRole('tooltip-trigger');
+    fireEvent.mouseEnter(tooltipTrigger);
+    await tooltipWait();
+
+    const tooltip = queryByText('Tooltip Text');
+    const tooltipRect = tooltip?.getBoundingClientRect();
+    const triggerRect = tooltipTrigger.getBoundingClientRect();
+
+    expect(tooltip).toBeInTheDocument();
+    expect(tooltipRect?.top).toBeLessThanOrEqual(triggerRect.top);
+    expect(tooltipRect?.left).toBeLessThanOrEqual(triggerRect.left);
 });
