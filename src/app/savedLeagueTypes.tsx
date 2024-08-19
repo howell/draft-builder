1d388fbe08803dbfb5b2bca2c2997778b56e57f6
Sam Caldwell <samcaldwell19@gmail.com>
2024-08-19 14:41:59 -0400

    sync up schema version numbers bc leagues prev used mocks number

--- src/app/savedLeagueTypes.tsx
+++ src/app/savedLeagueTypes.tsx
@@ -1,6 +1,6 @@
 import { PlatformLeague } from "@/platforms/common";
 
-export const CURRENT_LEAGUES_SCHEMA_VERSION = 0;
+export const CURRENT_LEAGUES_SCHEMA_VERSION = 2;
 
 export type StoredLeagues = {
     [leagueID: number]: PlatformLeague;
