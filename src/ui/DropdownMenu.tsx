67278e341dd4d875b993819bf2eae711590b1586
Sam Caldwell <samcaldwell19@gmail.com>
2024-08-26 11:47:35 -0400

    make DropdownMenus more flexible

--- src/ui/DropdownMenu.tsx
+++ src/ui/DropdownMenu.tsx
@@ -1,16 +1,37 @@
-import React, { useState, useEffect, useCallback, ReactElement } from 'react';
+import React, { useState, useEffect, useCallback, useRef, } from 'react';
 
 interface DropdownMenuProps {
-  options: { name: ReactElement, value: any  }[];
+  options: { name: React.ReactNode, value: any  }[];
   selectedOption: any;
-  onSelect: (name: ReactElement, value: any) => void;
+  onSelect: (name: React.ReactNode, value: any) => void;
+  styles?: DropdownStyleOptions;
 }
 
-const DropdownMenu: React.FC<DropdownMenuProps> = ({ options, selectedOption, onSelect }) => {
+export type DropdownStyleOptions = {
+    bgColor: string;
+    textColor: string;
+    hoverBgColor: string;
+    hoverTextColor: string;
+    border: string;
+}
+
+export const DEFAULT_DROPDOWN_STYLE: DropdownStyleOptions = {
+    bgColor: 'bg-gray-800',
+    textColor: 'text-white',
+    hoverBgColor: 'bg-gray-500',
+    hoverTextColor: 'text-black',
+    border: 'border border-gray-300',
+};
+
+const DropdownMenu: React.FC<DropdownMenuProps> = ({ options, selectedOption, onSelect, styles }) => {
+    styles = styles ?? DEFAULT_DROPDOWN_STYLE;
+    const hoverStyle = `hover:${styles.hoverBgColor} hover:${styles.hoverTextColor}`;
     const [isOpen, setIsOpen] = useState(false);
     const [highlightedIndex, setHighlightedIndex] = useState(-1);
+    const [isFocused, setIsFocused] = useState(false);
+    const parentRef = useRef<HTMLDivElement>(null);
 
-    const handleSelect = useCallback((name: ReactElement, value: any) => {
+    const handleSelect = useCallback((name: React.ReactNode, value: any) => {
         onSelect(name, value);
         setIsOpen(false);
     }, [onSelect, setIsOpen]);
@@ -23,7 +44,7 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({ options, selectedOption, on
 
     useEffect(() => {
         const handleKeyDown = (event: globalThis.KeyboardEvent) => {
-            if (isOpen) {
+            if (parentRef.current && isFocused && isOpen) {
                 if (event.key === 'ArrowDown') {
                     event.preventDefault();
                     setHighlightedIndex((prevIndex) => (prevIndex + 1) % options.length);
@@ -46,25 +67,35 @@ const DropdownMenu: React.FC<DropdownMenuProps> = ({ options, selectedOption, on
                 window.removeEventListener('keydown', handleKeyDown);
             };
         }
-    }, [isOpen, highlightedIndex, handleSelect, options]);
+    }, [isOpen, isFocused, highlightedIndex, handleSelect, options]);
 
 
     return (
-        <div className="relative inline-block w-full h-9 border border-gray-300 p-1">
-            <div className="w-full h-full p-2 cursor-pointer flex justify-between items-center"
-                onClick={() => setIsOpen(!isOpen)}
-                onBlur={() => setIsOpen(false)}>
+        <div className={`relative inline-block w-full h-9 border p-1 ${styles.border} `}
+            ref={parentRef}>
+            <div className={`w-full h-full p-2 cursor-pointer flex justify-between items-center
+             ${styles.bgColor} ${styles.textColor} `}
+                tabIndex={0}
+                onClick={() => { setIsOpen(!isOpen); setIsFocused(true); }}
+                onFocus={() => setIsFocused(true)}
+                onBlur={() => { setIsOpen(false); setIsFocused(false); }}
+                >
                 {options.find(option => option.value === selectedOption)?.name || ''}
                     <div className="absolute top-1 right-2">
                         <i className={`fas fa-chevron-down`} />
                     </div>
             </div>
             {isOpen && (
-                <div className="absolute top-full left-0 w-full border border-gray-300 bg-gray-800 z-50">
+                <div className={`absolute top-full left-0 w-full z-50
+                     ${styles.border}
+                     ${styles.bgColor}
+                     ${styles.textColor}`}>
                     <ul className="w-full max-h-52 overflow-y-auto">
-                        {options.map(option => (
-                            <li key={option.value}
-                                className={`p-1 cursor-pointer hover:bg-gray-500 ${highlightedIndex === options.indexOf(option) ? 'bg-gray-500' : ''}`}
+                        {options.map((option, i) => (
+                            <li key={`option-${i}`}
+                                className={`p-1 cursor-pointer
+                                    ${hoverStyle}
+                                    ${highlightedIndex === options.indexOf(option) ? `${styles.hoverBgColor} ${styles.hoverTextColor}` : ''}`}
                                 onClick={() => handleSelect(option.name, option.value)}>
                                 {option.name}
                             </li>
