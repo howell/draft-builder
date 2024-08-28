f15a58b07af8670415cac6e54db948b88ec02149
Sam Caldwell <samcaldwell19@gmail.com>
2024-08-27 21:24:09 -0400

    don't use redis tls in development environment

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,11 +1,14 @@
 import Redis from 'ioredis';
 
-const redis = new Redis(process.env.KV_URL!, {
-    tls: {
-        rejectUnauthorized: true
-    }
-}
+const isDevelopment = process.env.NODE_ENV === 'development';
 
-);
+const redis = isDevelopment ?
+    new Redis(process.env.KV_URL!)
+    : new Redis(process.env.KV_URL!, {
+        tls: {
+            rejectUnauthorized: true
+        }
+    }
+    );
 
 export default redis;
