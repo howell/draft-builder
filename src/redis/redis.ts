276e572ce432d1a524e20ec5e1aa4cc62b6caf9a
Sam Caldwell <samcaldwell19@gmail.com>
2024-08-27 20:56:05 -0400

    redis logging

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,5 +1,7 @@
 import Redis from 'ioredis';
 
+console.log("Connecting to redis url:", process.env.KV_URL);
 const redis = new Redis(process.env.KV_URL!);
+console.log("Redis status:", redis.status)
 
 export default redis;
