3b7401783f5e25b680b631952776dfef776ff84d
Sam Caldwell <samcaldwell19@gmail.com>
2024-08-27 21:37:37 -0400

    fix build

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,8 +1,2 @@
 import '@testing-library/jest-dom/matchers';
 import '@testing-library/jest-dom';
-import redis from '@/redis/redis';
-import { after } from 'node:test';
-
-afterAll(() => {
-    redis.quit();
-});
