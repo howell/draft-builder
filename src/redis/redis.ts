c2b3da15432ce95a0c48094d477325455d54cab8
Sam Caldwell <samcaldwell19@gmail.com>
2024-08-27 21:08:32 -0400

    tls options for redis

--- src/redis/redis.ts
+++ src/redis/redis.ts
@@ -1,7 +1,11 @@
 import Redis from 'ioredis';
 
-console.log("Connecting to redis url:", process.env.KV_URL);
-const redis = new Redis(process.env.KV_URL!);
-console.log("Redis status:", redis.status)
+const redis = new Redis(process.env.KV_URL!, {
+    tls: {
+        rejectUnauthorized: true
+    }
+}
+
+);
 
 export default redis;
